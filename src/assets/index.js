48a298e8b4506caf7c3705126c5f42432e865bfc
Allen <genelsisallenabriol@gmail.com>
2024-03-30 07:08:28 +0800

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,98 @@
+import logo from "./logo.svg";
+import myLogo from "./myLogo.svg"
+import myLogo2 from "./myLogo2.png"
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+import optum from "./company/Optum.png";
+import noLogo from "./company/noLogo.png"
+
+
+import yourai from "./yourai.png"
+import shoeshop from "./shoeshop.png"
+import appleclone from "./appleclone.png"
+
+import youraisvg from "./yourai.svg"
+import shoeshopsvg from "./shoeshop.svg"
+import appleclonesvg  from "./appleclone.svg"
+
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import portfolio2 from "./portfolio2.png";
+
+import gsap from "./tech/gsap.png";
+
+import portfolioLogo from "./portfolioLogo.ico";
+
+export {
+  logo,
+  myLogo,
+  myLogo2,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  optum,
+  noLogo,
+  carrent,
+  jobit,
+  tripguide,
+
+  yourai,
+  shoeshop,
+  appleclone,
+
+  youraisvg,
+  shoeshopsvg,
+  appleclonesvg,
+
+  portfolio2,
+  
+  gsap,
+  portfolioLogo,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,333 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    optum,
+    noLogo,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+
+    yourai,
+    shoeshop,
+    appleclone,
+
+    youraisvg,
+    shoeshopsvg,
+    appleclonesvg,
+
+    portfolio2,
+    
+    portfolioLogo,
+
+    gsap,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "home",
+      title: "Home",
+    },
+    
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "projects",
+      title: "Projects",
+    },
+    {
+      id: "testimonials",
+      title: "Testimonials",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Developer",
+      icon: mobile,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "GSAP",
+      icon: gsap,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Health Care Advocate & Fax/Mail Scheduler",
+      company_name: "Optum",
+      icon: optum,
+      iconBg: "#FF5F1F",
+      date: "March 2020 - April 2021",
+      points: [
+        "I'm a former Health Care Advocate of Optum Global Solutions for more than a year. And in this work I've managed to communicate to different members from different states of US. ",
+        "Answering every calls with the heart to help solved the problem of the members, giving them the solution they need to make their day better.",
+        "Giving enough and needed information for them to grasp everything they need to be familiar with their health care plan and maximize every benefit they have.",
+        "Working as a Health Care Advocate for more than a year makes me realize that a job can also be a life saving opportunity to someone in need of help.",
+      ],
+    },
+    {
+      title: "Game Developer/Lead Programmer",
+      company_name: "House of Vallinder",
+      icon: noLogo,
+      iconBg: "#FFFFFF",
+      date: "Jan 2021 - Feb 2022",
+      points: [
+        "I work as a Lead Programmer and 3D Designer on this Company to create a Unity Android Game.",
+        "I managed to create a Horror Game And Also a Board game for 2 years.",
+        "I designed the main game and programmed it.",
+        "And also one of the 3D Designer who designed the characters of the game.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sample Testimonial 1",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Sample Testimonial 2",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Allen optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Sample Testimonial 3",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+     
+      name: "YourAI",
+      description:
+        "Is a website that uses modern User Interface design. It also uses modern design to show prices of every services that is offered. It also uses bento box design UI to show other important information on the website.",
+      tags: [
+        {
+          name: "html",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        // {
+        //   name: "mongodb",
+        //   color: "green-text-gradient",
+        // },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      icon: youraisvg,
+      image: yourai,
+      source_code_link: "https://meizzelvanillafrappe.github.io/yourai/",
+    },
+    {
+    
+      name: "ShoeShop",
+      description:
+        "A Landing page for a shoes brand online website. This website uses the power of Tailwind Design as well as the React framework to make a beautiful website to showcase your products.",
+      tags: [
+        {
+          name: "html",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        // {
+        //   name: "restapi",
+        //   color: "green-text-gradient",
+        // },
+        // {
+        //   name: "scss",
+        //   color: "pink-text-gradient",
+        // },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      icon: shoeshopsvg,
+      image: shoeshop,
+      source_code_link: "https://meizzelvanillafrappe.github.io/shoeshop/",
+    },
+    {
+     
+      name: "Apple Like Website",
+      description:
+        "A UI Design inspired by the Apple cellphone brand website. This website uses the power of GSAP framework for the animation of its contents. And for the design it uses Tailwind.",
+      tags: [
+        {
+          name: "html",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        // {
+        //   name: "nextjs",
+        //   color: "blue-text-gradient",
+        // },
+       
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "gsap",
+          color: "green-text-gradient",
+        },
+      ],
+      icon: appleclonesvg,
+      image: appleclone,
+      source_code_link: "https://meizzelvanillafrappe.github.io/appleclone/",
+    },
+    {
+      name: "3D World Portfolio Website",
+      description:
+        "A portfolio Website Created using the combination of React and Tailwind CSS. It shows a 3D world with animated objects and pop ups as you reach a certain point. Use the Arrow keys or your mouse to move the Airplane around the 3D World.",
+      tags: [
+        // {
+        //   name: "nextjs",
+        //   color: "blue-text-gradient",
+        // },
+        {
+          name: "html",
+          color: "orange-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "javascript",
+          color: "yellow-text-gradient",
+        },
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        // {
+        //   name: "gsap",
+        //   color: "green-text-gradient",
+        // },
+        // {
+        //   name: "css",
+        //   color: "pink-text-gradient",
+        // },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "threejs",
+          color: "green-text-gradient",
+        },
+      ],
+      icon: portfolioLogo,
+      image: portfolio2,
+      source_code_link: "https://meizzelvanillafrappe.github.io/genelsis-portfolio-2/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
